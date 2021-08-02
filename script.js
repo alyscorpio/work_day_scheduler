6b38dabf0a4a6034e784e38cbd86c29aeba57013
Alys Scorpio <alyssad1182@gmail.com>
2021-08-02 15:39:06 -0700

    Getting current day and date using moment.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var time = $('currentDay');
 var saveButton = document.querySelectorAll('.saveBtn');
 var activity = document.querySelector('.activity');
 console.log(saveButton)
+
+// Current day/date from moment.js
+var today = moment().format('dddd, MMMM Do YYYY');
+timeDisplay.text(today);
