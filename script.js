17d3b3aae53a87fc065c106a3ca06f983418125a
Alys Scorpio <alyssad1182@gmail.com>
2021-08-02 15:45:54 -0700

    Creating functions for save button and storage

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ console.log(saveButton)
 // Current day/date from moment.js
 var today = moment().format('dddd, MMMM Do YYYY');
 timeDisplay.text(today);
+
+for (i = 0; i < saveButton.length; i++){
+    saveButton[i].addEventListener("click", storeActivity);
+}
+
+function storeActivity(){
+    var description = $(this).siblings(".activity").val()
+    console.log(description)
+    var time = $(this).parent().attr("class")
+    console.log(typeof time)
+    localStorage.setItem(time.split('')[1], description);
+}
