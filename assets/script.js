0fd2abbbbde1038a6544d32c3aec1e57007564bd
Alys Scorpio <alyssad1182@gmail.com>
2021-08-03 20:21:44 -0700

    fixed local storage issues

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,53 @@
-var time = $('currentDay');
-var saveButton = document.querySelectorAll('.saveBtn');
-var activity = document.querySelector('.activity');
-console.log(saveButton)
+// Variables from HTML elements
+var saveBtn = document.querySelectorAll('.saveBtn');
+console.log(saveBtn);
+var textInput = document.querySelector('.activity')
+var timeDisplay = $('currentDay');
+var thisTime = $('currentTime');
 
-// Current day/date from moment.js
+// Current date from moment.js
 var today = moment().format('dddd, MMMM Do YYYY');
-time.text(today);
+timeDisplay.text(today);
 
-for (i = 0; i < saveButton.length; i++){
-    saveButton[i].addEventListener("click", storeActivity);
+for (i = 0; i < saveBtn.length; i++){
+    saveBtn[i].addEventListener("click", storeInput);
 }
 
-function storeActivity(){
+function storeInput(){
     var description = $(this).siblings(".activity").val()
     console.log(description)
     var time = $(this).parent().attr("class")
     console.log(typeof time)
-    localStorage.setItem(time.split('')[1], description);
+    localStorage.setItem(time.split(' ')[1], description);
 }
 
 $("#09 + .activity").val(localStorage.getItem("9am"))
 $("#10 + .activity").val(localStorage.getItem("10am"))
 $("#11 + .activity").val(localStorage.getItem("11am"))
 $("#12 + .activity").val(localStorage.getItem("12pm"))
-$("#01 + .activity").val(localStorage.getItem("1pm"))
-$("#02 + .activity").val(localStorage.getItem("2pm"))
-$("#03 + .activity").val(localStorage.getItem("3pm"))
-$("#04 + .activity").val(localStorage.getItem("4pm"))
-$("#05 + .activity").val(localStorage.getItem("5pm"))
+$("#13 + .activity").val(localStorage.getItem("13pm"))
+$("#14 + .activity").val(localStorage.getItem("14pm"))
+$("#15 + .activity").val(localStorage.getItem("15pm"))
+$("#16 + .activity").val(localStorage.getItem("16pm"))
+$("#17 + .activity").val(localStorage.getItem("17pm"))
 
-function hour(){
+function blockHour(){
     var now = moment().hours();
-
-    $(".row").each(function() {
-        var timeBlock = parseInt($(this).attr("id"))
-
-        if (now > timeBlock){
-            $(this).addClass("past")
-        } else if (now === timeBlock){
-            $(this).removeClass("past")
-            $(this).addClass("present")
-        } else {
-            $(this).removeClass("past")
-            $(this).removeClass("present")
-            $(this).addClass("future")
-        }
+ 
+    $(".row").each(function(){
+       var blockTime = parseInt($(this).attr("id"))
+ 
+       if (now > blockTime){
+          $(this).addClass("past")
+       } else if (now === blockTime){
+          $(this).removeClass("past")
+          $(this).addClass("present")
+       } else {
+       $(this).removeClass("past")
+       $(this).removeClass("present")
+       $(this).addClass("future")
+       }
     })
 }
 
-hour();
+blockHour();
